f235509dbd639476cda01b4e6faae790ad4752b5
Hasan Demirtaş <utsukushihito@outlook.com>
2021-06-30 19:56:16 +0300

    create owner and member methods. (#33)

--- src/main/java/tr/com/infumia/claimplugin/paper/api/member/MemberCreator.java
+++ src/main/java/tr/com/infumia/claimplugin/paper/api/member/MemberCreator.java
@@ -1,13 +1,30 @@
 package tr.com.infumia.claimplugin.paper.api.member;
 
 import java.util.UUID;
-import java.util.function.Function;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * an interface to determine member creators.
  */
-@FunctionalInterface
-public interface MemberCreator extends Function<@NotNull UUID, @NotNull Member> {
+public interface MemberCreator {
 
+  /**
+   * creates a claim member.
+   *
+   * @param uniqueId the unique id to create.
+   *
+   * @return a newly created claim member.
+   */
+  @NotNull
+  Member createMember(@NotNull UUID uniqueId);
+
+  /**
+   * creates a claim owner.
+   *
+   * @param uniqueId the unique id to create.
+   *
+   * @return a newly created claim owner.
+   */
+  @NotNull
+  Member createOwner(@NotNull UUID uniqueId);
 }
