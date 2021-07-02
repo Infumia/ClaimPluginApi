3b28da8c30f93cfcabf3177e7903f14de2fa6b30
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-02 16:10:39 +0300

    Dev (#43)

---
+++ src/main/java/tr/com/infumia/claimplugin/paper/api/claim/Home.java
@@ -0,0 +1,77 @@
+package tr.com.infumia.claimplugin.paper.api.claim;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.bukkit.Location;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * an interface to determine homes.
+ */
+public interface Home {
+
+  /**
+   * creates a simple home instance.
+   *
+   * @param name the name to create.
+   * @param location the location to create.
+   *
+   * @return a newly created home instance.
+   */
+  static Home of(@NotNull final String name, @NotNull final Location location) {
+    return new Impl(location, name);
+  }
+
+  /**
+   * obtains the location.
+   *
+   * @return location.
+   */
+  @NotNull
+  Location getLocation();
+
+  /**
+   * sets the location.
+   *
+   * @param location the location to set.
+   */
+  void setLocation(@NotNull Location location);
+
+  /**
+   * obtains the name.
+   *
+   * @return name.
+   */
+  @NotNull
+  String getName();
+
+  /**
+   * sets the name.
+   *
+   * @param name the name to set.
+   */
+  void setName(@NotNull String name);
+
+  /**
+   * a simple implementation of {@link Home}.
+   */
+  @Getter
+  @Setter
+  @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+  final class Impl implements Home {
+
+    /**
+     * the location.
+     */
+    @NotNull
+    private Location location;
+
+    /**
+     * the name.
+     */
+    @NotNull
+    private String name;
+  }
+}
