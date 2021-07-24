a6e2fb944bb4e81621c7ad79c54e77eb6d7835a4
Hasan Demirtaş <utsukushihito@outlook.com>
2021-07-24 18:04:24 +0300

    format.

--- src/main/java/tr/com/infumia/claimplugin/paper/api/event/ClaimPanelOpenEvent.java
+++ src/main/java/tr/com/infumia/claimplugin/paper/api/event/ClaimPanelOpenEvent.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 import tr.com.infumia.claimplugin.paper.api.claim.ParentClaim;
 
@@ -11,6 +12,11 @@ import tr.com.infumia.claimplugin.paper.api.claim.ParentClaim;
  * a class that represents claim panel open events that fire when someone tries to open the claim menu.
  */
 public final class ClaimPanelOpenEvent extends ClaimEvent implements Cancellable {
+
+  /**
+   * the handler list.
+   */
+  private static final HandlerList handlerList = new HandlerList();
 
   /**
    * the player.
@@ -35,5 +41,21 @@ public final class ClaimPanelOpenEvent extends ClaimEvent implements Cancellable
   public ClaimPanelOpenEvent(@NotNull final ParentClaim claim, @NotNull final Player player) {
     super(false, claim);
     this.player = player;
+  }
+
+  /**
+   * the handler list.
+   *
+   * @return handler list.
+   */
+  @NotNull
+  public static HandlerList getHandlerList() {
+    return ClaimPanelOpenEvent.handlerList;
+  }
+
+  @NotNull
+  @Override
+  public final HandlerList getHandlers() {
+    return ClaimPanelOpenEvent.handlerList;
   }
 }
--- src/main/java/tr/com/infumia/claimplugin/paper/api/event/ClaimPreDeleteEvent.java
+++ src/main/java/tr/com/infumia/claimplugin/paper/api/event/ClaimPreDeleteEvent.java
@@ -3,6 +3,7 @@ package tr.com.infumia.claimplugin.paper.api.event;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 import tr.com.infumia.claimplugin.paper.api.claim.ParentClaim;
 
@@ -10,6 +11,11 @@ import tr.com.infumia.claimplugin.paper.api.claim.ParentClaim;
  * a class that represents claim pre delete events that fire before the claim delete.
  */
 public final class ClaimPreDeleteEvent extends ClaimEvent implements Cancellable {
+
+  /**
+   * the handler list.
+   */
+  private static final HandlerList handlerList = new HandlerList();
 
   /**
    * the cancelled.
@@ -25,5 +31,21 @@ public final class ClaimPreDeleteEvent extends ClaimEvent implements Cancellable
    */
   public ClaimPreDeleteEvent(@NotNull final ParentClaim claim) {
     super(claim);
+  }
+
+  /**
+   * the handler list.
+   *
+   * @return handler list.
+   */
+  @NotNull
+  public static HandlerList getHandlerList() {
+    return ClaimPreDeleteEvent.handlerList;
+  }
+
+  @NotNull
+  @Override
+  public final HandlerList getHandlers() {
+    return ClaimPreDeleteEvent.handlerList;
   }
 }
