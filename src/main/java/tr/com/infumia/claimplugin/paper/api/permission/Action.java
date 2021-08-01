9c05d5acad29846f834bcab1439189b09a921fa5
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-01 14:44:38 +0300

    make it bi consumer.

--- src/main/java/tr/com/infumia/claimplugin/paper/api/permission/Action.java
+++ src/main/java/tr/com/infumia/claimplugin/paper/api/permission/Action.java
@@ -1,13 +1,13 @@
 package tr.com.infumia.claimplugin.paper.api.permission;
 
-import java.util.function.Consumer;
+import java.util.function.BiConsumer;
 import org.jetbrains.annotations.NotNull;
 import tr.com.infumia.claimplugin.paper.api.event.LocationalEvent;
 
 /**
  * an interface to determine actions for each {@link ControlResult}.
  */
-public interface Action extends Consumer<@NotNull LocationalEvent> {
+public interface Action extends BiConsumer<@NotNull ControlResult, @NotNull LocationalEvent> {
 
   /**
    * registers the action.
